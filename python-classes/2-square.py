ed41ec541c130b337f1e0ab7c3edd0a245efcb10
root <6328@holbertonstudents.com>
2023-06-14 10:28:16 -0700

    last commit task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python interpreter """
+
+
+class Square:
+    """ class: Square """
+    __size = 0
+
+    def __init__(self, size=0):
+        """Init with size of square"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+    self.__size = size
