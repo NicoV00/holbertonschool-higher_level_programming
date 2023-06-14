bf658d43ad051b16a86363949bc1470a454b7b7e
root <6328@holbertonstudents.com>
2023-06-14 07:51:29 -0700

    classes fixed pycode

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Empty class created"""
+
+
 class Square:
-    """Square class defined an object"""
-    def __init__(self):
-        """Square class"""
-        pass
+    """Square class"""
+    pass
