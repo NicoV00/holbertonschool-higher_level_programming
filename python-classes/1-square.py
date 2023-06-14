63ca959961e17a4eac38689058946d07350cebf1
root <6328@holbertonstudents.com>
2023-06-14 10:11:39 -0700

    last commit task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""python interpreter"""
+
+
+class Square:
+    """ square class """
+
+        def __init__(Self, size):
+                """initialize class"""
+                Self.__size = size
