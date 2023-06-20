4928d86652f5cc9c7def58fe5b9a3f7fe0efe058
francisco.valles <franval95@gmail.com>
2023-06-20 10:46:45 -0700

    task done last commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,23 @@
-#!/usr/bin/python3
 class Square:
-    """creates square object"""
-
+    """Define a square object"""
     def __init__(self, size=0):
-        self.__size = size
-    """initializes a square object with size Args"""
+        """Initializes a new instance of the square class with a given size"""
+        self.size = size
 
     @property
     def size(self):
+        """Gets the size of the square"""
         return self.__size
-    """gets size of square"""
 
     @size.setter
     def size(self, value):
-        if (type(value) is not int):
+        """Sets the size of the square"""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        """sets size of square square"""
 
     def area(self):
-        return (self.__size**2)
-        """return area of square based on size"""
+        """Calculates the area of the square"""
+        return self.size ** 2
