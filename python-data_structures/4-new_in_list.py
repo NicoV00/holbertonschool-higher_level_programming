9e909a05a31483405d6cad759050fd7972256547
NicoV00 <6328@holbertonstudents.com>
2023-09-24 19:39:02 -0700

    task 4 finally fixed

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copylist = my_list.copy()
-    if idx < 1:
+    if idx < 0:
         return copylist
     elif idx > len(my_list) - 1:
         return copylist
