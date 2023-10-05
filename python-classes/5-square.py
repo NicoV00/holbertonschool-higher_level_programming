48b21fec5a17ff4aa00f608d241f85d39978682a
NicoV00 <6328@holbertonstudents.com>
2023-10-05 09:38:53 -0700

    task 5 fixed and uploaded

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        for _ in range(0, self.__size):
+            for _ in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
