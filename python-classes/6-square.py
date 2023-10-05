006bb175110477072e2a0f7956e1b93ca834cacb
NicoV00 <6328@holbertonstudents.com>
2023-10-05 10:00:01 -0700

    task 6. Coordinates of a square uploaded fixed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -57,6 +57,6 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if any(not isinstance(pos, int) for pos in position):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[1] < 0 or position[0] < 0:
+        if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position= position
+        self.__position = position
