af2d6c99dd487fd5947a9ee4accadc82b342ad31
NicoV00 <6328@holbertonstudents.com>
2023-10-10 12:42:33 -0700

    uploaded task-2 and fixed tests

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Created task 2"""
+
+
+def matrix_divided(matrix, div):
+    """Divides the matrix."""
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if (not isinstance(matrix, list) or not
+       all(isinstance(elem, list) for elem in matrix)):
+        raise TypeError(msg)
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    new_list = []
+    len_row = len(matrix[0])
+    for elem in matrix:
+        if len_row != len(elem):
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in elem:
+            if isinstance(num, (int, float)):
+                new_list.append(round(num / div, 2))
+            else:
+                raise TypeError(msg)
+        new_matrix.append(new_list)
+        new_list = []
+
+    return new_matrix
