ab931f690738a33ecac84a94f5769d53a61b0c5e
NicoV00 <6328@holbertonstudents.com>
2023-10-10 14:07:10 -0700

    added task 5 tests fixed

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Created task 5"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    special_characters = ['.', '?', ':']
+
+    actual_line = ""
+
+    for char in text:
+        actual_line += char
+
+        if char in special_characters:
+            print(actual_line.strip())
+            print()
+            actual_line = ""
+
+    if actual_line:
+        print(actual_line.strip())
