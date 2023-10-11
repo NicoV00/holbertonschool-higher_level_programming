f803c53893e66aff531fe8cb847c5e9d85d34638
NicoV00 <6328@holbertonstudents.com>
2023-10-11 10:31:54 -0700

    More classes tasks uploaded fixed

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Task 0 """
+"""Task 0"""
 
 
 class Rectangle:
-    """ Defines rectangle """
-    
-    pass
+    """makes rectangle object"""
+    def __init__(self):
+        pass
