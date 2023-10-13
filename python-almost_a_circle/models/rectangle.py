9e25eec3821ebad0a5939d586e3475df1b0a96fa
NicoV00 <6328@holbertonstudents.com>
2023-10-13 15:40:32 -0700

    added area and display to rectangle py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -39,7 +39,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -50,7 +50,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """x setter"""
-        self.x_valid(x)
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be > 0")
         self.__x = x
 
     @property
@@ -61,7 +64,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y setter"""
-        self.y_valid(y)
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be > 0")
         self.__y = y
 
     def area(self):
@@ -74,3 +80,70 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def display(self):
+        "Prints in stdout the Rectangle instance"
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        "Returns a str representation"
+        return (
+            f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+            f" - {self.__width}/{self.__height}"
+        )
+
+    def update(self, *args, **kwargs):
+        "Assigns an arg to each attribute"
+        if args and len(args) != 0:
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.__height = args[2]
+                self.__x = args[3]
+                self.__y = args[4]
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def display(self):
+        "Prints in stdout the Rectangle instance"
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        "Returns a str representation"
+        return (
+            f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+            f" - {self.__width}/{self.__height}"
+        )
+
+    def update(self, *args, **kwargs):
+        "Assigns an arg to each attribute"
+        if args and len(args) != 0:
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.__height = args[2]
+                self.__x = args[3]
+                self.__y = args[4]
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+                
