7cc7e7cb525aca271ee898f8e0dbbadf2dca6e41
NicoV00 <6328@holbertonstudents.com>
2023-11-15 11:36:34 -0800

    added wrong, now corrected

--- SQL_more_queries/6-states.sql
+++
@@ -1,4 +0,0 @@
--- creates user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-ALTER USER IF EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
