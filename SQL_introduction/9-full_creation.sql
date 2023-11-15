98b03ddec4dff9074b1e5dbe9f68b20f0e4477aa
NicoV00 <6328@holbertonstudents.com>
2023-11-15 10:26:44 -0800

    uploaded task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- Script that creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOS EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO secont_table
+INSERT INTO second_table (id, name, score)
 VALUES
 (1, 'Jhon', 10)
 (2, 'Alex', 3)
