a4ac8987b48f3adec60151aa00d05f3c2f9857db
NicoV00 <6328@holbertonstudents.com>
2023-11-17 14:50:37 -0800

    fixed syntax error

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT NAME AS genre, COUNT(id) As number_of_showS
+SELECT NAME AS genre, COUNT(id) As number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON genre_id = id
 GROUP BY genre
