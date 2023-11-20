b94a7f5e41f40330e38b018e430ccef42d474022
NicoV00 <6328@holbertonstudents.com>
2023-11-20 13:19:19 -0800

    fixed pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,15 +2,15 @@
 """task 8"""
 
 
-BaseGeometry = __import__("7-base_geomtry").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Class representing a rectangle."""
 
     def __init__(self, width, height):
-        """init rectangle"""
+        """Init Rectangle."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__width =  width
-        self.__height =  height
+        self.__width = width
+        self.__height = height
