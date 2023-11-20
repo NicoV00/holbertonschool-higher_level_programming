93531007a3fa1564f28fa8da5429705b52f4ad88
NicoV00 <6328@holbertonstudents.com>
2023-11-19 19:00:33 -0800

    added task

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' script that lists all cities from the database hbtn_0e_4_usaimport MySQLdb.'''
 import sys
+import MySQLdb
 if __name__ == "__main__":
     """main function"""
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
@@ -9,7 +10,8 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute('''SELECT cities.id, cities.name, states.name
-                FROM cities JOIN states
+                FROM cities
+                JOIN states
                 ON state_id = states.id ORDER BY cities.id''')
     fetchs = cur.fetchall()
     for x in range(len(fetchs)):
