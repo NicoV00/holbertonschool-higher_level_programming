f586ffd2494e6856e9b3e56fff6a0839e923ce1f
NicoV00 <6328@holbertonstudents.com>
2023-11-20 11:52:43 -0800

    added task

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task 4"""
+
+
+def inherits_from(obj, a_class):
+    """return"""
+    if type(obj) == a_class:
+        return False
+    
+    return issubclass(type(obj), a_class)
