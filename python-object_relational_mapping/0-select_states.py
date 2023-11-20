c2b15a4550303208968dcf67a8e9d64229dd38ab
NicoV00 <6328@holbertonstudents.com>
2023-11-19 16:55:52 -0800

    uploaded task 0 and readme

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# gets all states
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    try:
+        # Database connection
+        db_connection = MySQLdb.connect(
+            "localhost",
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3])
+    except MySQLdb.OperationalError:
+        print("Can't connect to database")
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db_connection.close()
