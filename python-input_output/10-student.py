dad4ab0975135a220f8527d6b5081371df6aba48
NicoV00 <6328@holbertonstudents.com>
2023-11-21 13:39:02 -0800

    added tasks, fixed styles

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""task 10"""
+
+
+class Student():
+    """student class created"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        new_dic = {}
+        if attrs is not None:
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    new_dic[att] = self.__dict__[att]
+            return new_dic
+        else:
+            return self.__dict__
