8d478b47b997c3fd975ab763a945c45d3ba52a2b
NicoV00 <6328@holbertonstudents.com>
2023-11-21 13:16:03 -0800

    testes, added task

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""task 5"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file, using a JSON representation:"""
+    with open(filename, mode='w') as j_son:
+        json.dump(my_obj, j_son)
