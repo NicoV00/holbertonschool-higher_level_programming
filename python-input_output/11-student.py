a5965b627b555d0ded466a651be1980c4709a3dd
NicoV00 <6328@holbertonstudents.com>
2023-11-22 14:47:22 -0800

    added task

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""task 10"""
+
+
+class Student():
+    """student class created"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        "Retrieves a dict representation of a Student instance"
+        if type(attrs) is list:
+            data = {}
+            for x in attrs:
+                if hasattr(self, x):
+                    data[x] = getattr(self, x)
+            return data
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance:"""
+        for key, value in json.items():
+            setattr(self, key, value)
