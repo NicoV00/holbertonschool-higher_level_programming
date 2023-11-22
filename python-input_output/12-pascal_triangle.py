46c879f1adc27e6b5bb5496b58851a4f3b71fcc5
NicoV00 <6328@holbertonstudents.com>
2023-11-22 14:58:02 -0800

    added pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task 12 pascal"""
+
+
+def pascal_triangle(n):
+    row = []
+    for i in range(n):
+        current_row = [1] * (i + 1)
+        row.append(current_row)
+    return row
