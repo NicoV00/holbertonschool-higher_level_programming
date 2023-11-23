88c9d23137bfb0c8bce131dde7b75bd21a35bd19
NicoV00 <6328@holbertonstudents.com>
2023-11-23 12:55:49 -0800

    fixed file

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """task 7"""
-if __name__ == "__main__":
-    import sys
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
